b4c518d7b9c77541c8fc4531075d7ab3e61ee1c8
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-01-20 23:26:02 -0500

    obtained the images and created the final screen

--- Game1.py
+++ Game1.py
@@ -49,9 +49,26 @@ def CreateBoard(num_rows,num_cols,num_cells):
         elif direction == 3: blank_cell_idx = moveD(board,blank_cell_idx,num_cols)
     return board, blank_cell_idx
 
+def GeImagePaths(rootdir):
+    imagenames = os.listdir(rootdir)
+    assert len(imagenames) > 0
+    return os.path.join(rootdir,random.choice(imagenames))
 
 
-
-
+def ShowEndInterface(screen,width,height):
+    screen.fill(cfg.BACKGROUNDCOLOR)
+    font = pygame.font.Font(cfg.FONTPATH, width/15)
+    title = font.render('Good Job! You Won!',True, (233,150,122))
+    rect = title.get_rect()
+    rect.midtop = (width/2, height/2.5)
+    screen.blit(title,rect)
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+        
 if __name__ == '__main__':
     main()
