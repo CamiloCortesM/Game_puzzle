d6e65fb997f8b2b656f22d438ba32b615b30bb2b
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-01-20 23:07:47 -0500

    the missing board and functions have been created

--- Game1.py
+++ Game1.py
@@ -13,17 +13,41 @@ def isGameOver(board,size):
         if board[i] != i: return False
     return True
 
-def moveR(board, black_cell_idx, num_cols):
-    if black_cell_idx%num_cols == 0: return black_cell_idx
-    board[black_cell_idx-1],board[black_cell_idx] = board[black_cell_idx],board[black_cell_idx-1]
-    return black_cell_idx-1
+def moveR(board, blank_cell_idx, num_cols):
+    if blank_cell_idx%num_cols == 0: return blank_cell_idx
+    board[blank_cell_idx-1],board[blank_cell_idx] = board[black_cell_idx],board[black_cell_idx-1]
+    return blank_cell_idx-1
 
-def moveL(board, black_cell_idx, num_cols):
-    if (black_cell_idx+1)%num_cols == 0: return black_cell_idx
-    board[black_cell_idx+1],board[black_cell_idx] = board[black_cell_idx],board[black_cell_idx+1]
-    return black_cell_idx+1
+def moveL(board, blank_cell_idx, num_cols):
+    if (blank_cell_idx+1)%num_cols == 0: return blank_cell_idx
+    board[blank_cell_idx+1],board[blank_cell_idx] = board[blank_cell_idx],board[blank_cell_idx+1]
+    return blank_cell_idx+1
+
+def moveD(board, blank_cell_idx, num_cols):
+    if blank_cell_idx<num_cols: return blank_cell_idx
+    board[blank_cell_idx-num_cols],board[blank_cell_idx] = board[blank_cell_idx],board[blank_cell_idx-num_cols]
+    return blank_cell_idx-num_cols
     
+def moveU(board, blank_cell_idx,num_rows, num_cols):
+    if blank_cell_idx>=(num_rows-1)*num_cols: return blank_cell_idx
+    board[blank_cell_idx+num_cols],board[blank_cell_idx] = board[blank_cell_idx],board[blank_cell_idx+num_cols]
+    return blank_cell_idx+num_cols
 
+def CreateBoard(num_rows,num_cols,num_cells):
+    board=[]
+    for i in range(num_cells): board.append(i)
+    
+    blank_cell_idx = num_cells-1
+    board[blank_cell_idx]=-1
+    
+    for i in range(cfg.RANDNUM):
+        direction = random.randint(0,3)
+        
+        if direction == 0: blank_cell_idx = moveL(board,blank_cell_idx,num_cols)
+        elif direction == 1: blank_cell_idx = moveR(board,blank_cell_idx,num_cols)
+        elif direction == 2: blank_cell_idx = moveU(board,blank_cell_idx,num_rows,num_cols)
+        elif direction == 3: blank_cell_idx = moveD(board,blank_cell_idx,num_cols)
+    return board, blank_cell_idx
 
 
 
